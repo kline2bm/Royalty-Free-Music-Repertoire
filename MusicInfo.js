87acc2b830e2c0f342bb187020bfba1e37bb432d
Bryce Kline <161635434+kline2bm@users.noreply.github.com>
2024-12-06 14:39:25 -0500

    Update MusicInfo.js
    added fields: raw, title, src, artist, emotion, instruments

--- MusicInfo.js
+++ MusicInfo.js
@@ -1,7 +1,23 @@
+/**
+ * Information about a piece of music
+ */
 class MusicInfo {
-  /* TODO
-  fields,
-  a private static field that must be passed to the constructor,
-  a static set field
-  */
+  // {string} the unprocessed string (e.g. "Accralate\\iKevin MacLeod\\cAPRC MRBA")
+  raw;
+
+  // {string} the title of the piece (e.g. "Accralate")
+  title;
+
+  // {string} the URL of a piece (e.g. "https://incompetech.com/music/royalty-free/mp3-royaltyfree/Accralate.mp3")
+  src;
+
+  // {string} the artist/composer of the piece (e.g. "Kevin MacLeod")
+  artist;
+
+  // {"angry"|"calm"|"fearful"|"happy"|"neutral"|"romantic"|"sad"} the (maybe subjective) emotion of a piece (e.g. "calm")
+  emotion;
+
+  // {string[]} an array of instruments in a piece (e.g. ["Acoustic Percussion", "Marimba"])
+  instruments;
 }
+// Title\sArtist\eINST
