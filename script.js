92368b8dc980bd25b13d20ccee3422cc3e81dab2
Bryce Kline <161635434+kline2bm@users.noreply.github.com>
2024-12-16 23:45:48 -0500

    Update script.js
    Tests for MusicInfo.makeNew and MusicInfo.retrieve
    TODO: Tests for MusicInfo.delete
    1. deleting existing (true)
    1.1. check that retrieving it throws error
    2. deleting just deleted (false)
    3. deleting never constructed (false)
    4. deleting constructed w/ addToMap = false (false)

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-const THE_BUTTON = document.getElementById("myButton");
-THE_BUTTON.onclick = function() {
-  alert("Hello, world!");
+console.group("Testing MusicInfo.makeNew");
+let ACCR = MusicInfo.makeNew("Accralate\\iKevin MacLeod\\cAPRC MRBA");
+try {
+  MusicInfo.makeNew("Accralate\\iKevin MacLeod\\cAPRC");
+  console.error("duplicate MusicInfo in map")
+} catch(e) {
+  console.assert(e.message.includes("Map already has"));
 }
+let ABC = MusicInfo.makeNew("A\\iB\\nEFFX", false), XYZ = MusicInfo.makeNew("A\\iB\\nHPRC", false);
+console.log("ACCR", ACCR); console.log("ABC", ABC); console.log("XYZ", XYZ);
+console.groupEnd();
+
+console.group("Testing MusicInfo.retrieve");
+console.log("ACCR-retrieved", MusicInfo.retrieve("Accralate\\i");
+try {
+  console.error(MusicInfo.retrieve("Does not exist\\i"));
+} finally {
+  MusicInfo.makeNew("Odyssey\\iKevin MacLeod\\hSYNZ");
+  console.log("ODYS-retrieved", MusicInfo.retrieve("Odyssey\\i"));
+}
+console.groupEnd();
+
+// TODO: test delete method
