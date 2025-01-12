c9007220d380d9fd791598c2f43dca899826b13e
Bryce Kline <161635434+kline2bm@users.noreply.github.com>
2025-01-12 17:55:57 -0500

    Update script.js
    fixed bug: added missing ')'

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ console.group("testing MusicInfo.delete method");
 let ABC = MusicInfo.makeNew("ABC\\iXYZ\nAPNO");
 console.assert(MusicInfo.delete("ABC\\i"));
 console.assert(!(MusicInfo.delete("Does not exist")));
-console.assert(!(MusicInfo.delete("ABC\\i"));
+console.assert(!(MusicInfo.delete("ABC\\i")));
 console.groupEnd();
