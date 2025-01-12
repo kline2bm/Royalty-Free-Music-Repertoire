8710a0c5bfd1d8d03fe1d5aa7431f6ded1113533
Bryce Kline <161635434+kline2bm@users.noreply.github.com>
2025-01-12 17:55:11 -0500

    Update script.js
    added tests for MusicInfo.delete method

--- script.js
+++ script.js
@@ -1,23 +1,7 @@
-console.group("Testing MusicInfo.makeNew");
-let ACCR = MusicInfo.makeNew("Accralate\\iKevin MacLeod\\cAPRC MRBA");
-try {
-  MusicInfo.makeNew("Accralate\\iKevin MacLeod\\cAPRC");
-  console.error("duplicate MusicInfo in map");
-} catch(e) {
-  console.assert(e.message.includes("Map already has"));
-}
-let ABC = MusicInfo.makeNew("A\\iB\\nEFFX", false), XYZ = MusicInfo.makeNew("A\\iB\\nHPRC", false);
-console.log("ACCR", ACCR); console.log("ABC", ABC); console.log("XYZ", XYZ);
-console.groupEnd();
-
-console.group("Testing MusicInfo.retrieve");
-console.log("ACCR-retrieved", MusicInfo.retrieve("Accralate\\i"));
-try {
-  console.error(MusicInfo.retrieve("Does not exist\\i"));
-} finally {
-  MusicInfo.makeNew("Odyssey\\iKevin MacLeod\\hSYNZ");
-  console.log("ODYS-retrieved", MusicInfo.retrieve("Odyssey\\i"));
-}
-console.groupEnd();
-
 // TODO: test delete method
+console.group("testing MusicInfo.delete method");
+let ABC = MusicInfo.makeNew("ABC\\iXYZ\nAPNO");
+console.assert(MusicInfo.delete("ABC\\i"));
+console.assert(!(MusicInfo.delete("Does not exist")));
+console.assert(!(MusicInfo.delete("ABC\\i"));
+console.groupEnd();
