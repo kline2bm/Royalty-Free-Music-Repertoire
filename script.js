1960480e81ce67425edb39341723e5162518f992
Bryce Kline <161635434+kline2bm@users.noreply.github.com>
2025-01-12 18:04:06 -0500

    Update script.js
    fixed bug: needed to escape second backslash; y'know what? maybe I should use a different delimiter

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // TODO: test delete method
 console.group("testing MusicInfo.delete method");
-let ABC = MusicInfo.makeNew("ABC\\iXYZ\nAPNO");
+let ABC = MusicInfo.makeNew("ABC\\iXYZ\\nAPNO");
 console.assert(MusicInfo.delete("ABC\\i"));
 console.assert(!(MusicInfo.delete("Does not exist")));
 console.assert(!(MusicInfo.delete("ABC\\i")));
