32123b3da71595c9168575b67b15f15c3e7bf173
Bryce Kline <161635434+kline2bm@users.noreply.github.com>
2025-01-12 18:05:10 -0500

    Update script.js
    log ABC

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // TODO: test delete method
 console.group("testing MusicInfo.delete method");
 let ABC = MusicInfo.makeNew("ABC\\iXYZ\\nAPNO");
+console.log("ABC", ABC);
 console.assert(MusicInfo.delete("ABC\\i"));
 console.assert(!(MusicInfo.delete("Does not exist")));
 console.assert(!(MusicInfo.delete("ABC\\i")));
